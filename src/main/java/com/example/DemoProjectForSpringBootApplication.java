c49f0c1e392aa2cfb5a885d0ec0bb798a192a00e
Sofia Hernandez <csofiah>
2022-07-23 18:50:49 +0200

    add files for spring batch schedule

--- src/main/java/com/example/DemoProjectForSpringBootApplication.java
+++ src/main/java/com/example/DemoProjectForSpringBootApplication.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoProjectForSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectForSpringBootApplication.class, args);
 	}
-
+ 
 }
